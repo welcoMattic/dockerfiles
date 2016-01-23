a567401387bb4ed7ba4c5f93dbf1c015e86564cc
xataz <xataz@xataz.net>
2016-01-23 20:10:59 +0100

    Edit 0bin Dockerfile && Add README.md

--- 0bin/settings.py
+++ 0bin/settings.py
@@ -2,18 +2,20 @@ from zerobin.default_settings import *
 DEBUG = True
 STATIC_FILES_ROOT = os.path.join(ROOT_DIR, 'static')
 
-PASTE_FILES_ROOT = os.path.join(ROOT_DIR, 'static', 'content')
 TEMPLATE_DIRS = (
     os.path.join(ROOT_DIR, 'views'),
 )
 HOST = "0.0.0.0"
 PORT = "8000"
-USER = None     
-GROUP = None     
+USER = "0bin"
+GROUP = "0bin"
+
+PASTE_FILES_ROOT = "/paste"
 
 MENU = (
-    ('Home', '/'),                                              
-    #<custom menu>                                                        
+    ('Home', '/'),
+    #<custom menu>
     ('Download 0bin', 'https://github.com/sametmax/0bin'),
-    ('Contact', 'mailto:<email>')
+    ('Contact', 'mailto:admin@domain.tld')
 )
+
