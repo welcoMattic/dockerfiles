bdcad964926433c2ff41edc3fb348eb4e3732373
xataz <xataz@xataz.net>
2016-04-15 15:26:44 +0200

    Update(ALL) : Test mais je suis pas sur ^^

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -e
 
 # this is kind of an expensive check, so let's not do this twice if we
 # are running more than one validate bundlescript
 VALIDATE_REPO='https://github.com/xataz/dockerfiles.git'
 VALIDATE_BRANCH='master'
+VALIDATE_USER='xataz'
+
 
 VALIDATE_HEAD="$(git rev-parse --verify HEAD)"
 
@@ -21,6 +22,44 @@ validate_diff() {
 	fi
 }
 
+
+deps_pull() {
+    image=$1
+    # alpine/3.3/.tags:3.3 latest
+
+
+    if [[ $image == ${VALIDATE_USER}* ]];then
+        local image_name=$(echo $image | sed 's|\(.*\)/\(.*\):\(.*\)|\2|g')
+        local image_tag=$(echo $image | sed 's|\(.*\)/\(.*\):\(.*\)|\3|g')
+        local image_path=$(dirname $(grep $image_tag $(find $image_name -type f -name .tags) | cut -d: -f1))
+    else
+        local image_name=$(echo $image | sed 's|\(.*\):\(.*\)|\1|g')
+        local image_tag=$(echo $image | sed 's|\(.*\):\(.*\)|\2|g')
+    fi
+
+    if [ -z $image_path ];then
+        deps_pull $image_name
+        docker build -t ${VALIDATE_USER}/${image_name}:${image_tag} ${image_path}
+    else
+        docker pull ${image_name}
+    fi
+}
+
+build_image() {
+    image_name=$1
+    image_dir=$2
+
+    if [ -x "${image_dir}/.tags" ]; then
+        tags_list=$(cat ${image_dir}/.tags)
+    else
+        tags_list="latest"
+    fi
+
+    for tag in $tags_list; do
+        docker build -t ${image_name}:${image_tag} ${image_dir}
+    done
+}
+
 # get the dockerfiles changed
 IFS=$'\n'
 files=( $(validate_diff --name-only -- '*Dockerfile') )
@@ -30,17 +69,10 @@ unset IFS
 for f in "${files[@]}"; do
 	image=${f%Dockerfile}
 	base=${image%%\/*}
-	suite=${image##*\/}
 	build_dir=$(dirname $f)
 
-	if [[ -z "$suite" ]]; then
-		suite=latest
-	fi
-
-	(
-	set -x
-	docker build -t ${base}:${suite} ${build_dir}
-	)
+    deps_pull $(grep 'FROM' ${build_dir}/Dockerfile | awk '{print $2}')
+    build_image ${base} ${build_dir}
 
 	echo "                       ---                                   "
 	echo "Successfully built ${base}:${suite} with context ${build_dir}"
