2a32dce7a2efb58bf8d0deec9b1ecbef0ef7b751
xataz <xataz@xataz.net>
2016-07-12 06:10:01 +0200

    Update(rutorrent) : Edit custom.sh

--- rutorrent/custom.sh
+++ rutorrent/custom.sh
@@ -4,15 +4,15 @@ FOLDER=$(dirname $0)
 DOCKER_PUSH=$1
 
 # Download dependencies
-docker pull alpine:3.3
-docker build -t xataz/alpine:3.3 alpine/3.3
+docker pull alpine:3.4
+docker build -t xataz/alpine:3.4 alpine/3.4
 
 # Build rutorrent
 docker build -t xataz/rutorrent:latest $FOLDER
 docker build --build-arg WITH_FILEBOT=YES -t xataz/rutorrent:filebot $FOLDER
 docker build --build-arg WITH_FILEBOT=YES -t xataz/rutorrent:latest-filebot $FOLDER
 
-if [ "$DOCKER_PUSH"== "push" ]; then
+if [ "$DOCKER_PUSH" == "push" ]; then
   for image in $(docker images --format "{{.Repository}}:{{.Tag}}" rutorrent); do
     docker push $image
   done
