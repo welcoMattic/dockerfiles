5ca9d32e6b4738fff6dfd339ea62e9b354c9c2f9
xataz <xataz@xataz.net>
2016-09-14 22:22:18 +0200

    Update(rutorrent) : Rename rutorrent to rtorrent-rutorrent

---
+++ rtorrent-rutorrent/custom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+FOLDER=$(dirname $0)
+DOCKER_PUSH=$1
+
+# Download dependencies
+docker pull xataz/s6-alpine:3.4
+
+# Build rtorrent-rutorrent
+docker build -t xataz/rtorrent-rutorrent:latest $FOLDER
+docker build --build-arg WITH_FILEBOT=YES -t xataz/rtorrent-rutorrent:filebot $FOLDER
+docker build --build-arg WITH_FILEBOT=YES -t xataz/rtorrent-rutorrent:latest-filebot $FOLDER
+
+if [ "$DOCKER_PUSH" == "push" ]; then
+  for tag in latest filebot latest-filebot; do
+    docker push rtorrent-rutorrent:$tag
+  done
+fi
