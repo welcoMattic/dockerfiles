60476af3400a7d21570a0eb2b6f518d81f7ff0b6
xataz <xataz@mondedie.fr>
2016-11-22 14:11:29 +0100

    Update(build.sh) : Update build script

--- build.sh
+++ build.sh
@@ -14,80 +14,14 @@ CGREEN="${CSI}1;32m"
 CYELLOW="${CSI}1;33m"
 CBLUE="${CSI}1;34m"
 
-HEAD="$(git rev-parse --verify HEAD)"
-
 git fetch -q "$REPO" "refs/heads/$BRANCH"
-UPSTREAM="$(git rev-parse --verify FETCH_HEAD)"
-
-COMMIT_DIFF="$UPSTREAM...$HEAD"
-
-validate_diff() {
-	if [ "$UPSTREAM" != "$HEAD" ]; then
-		git diff "$COMMIT_DIFF" "$@"
-	else
-		git diff HEAD~ "$@"
-	fi
-}
-
-build_image() {
-  image_name=$1
-  image_dir=$2
-  tags_list=${latest-$(grep 'tags=' $image_dir/Dockerfile | cut -d'"' -f2)}
-
-
-  for tag in $tags_list; do
-    echo -e "${CBUILD}Build ${USER}/${image_name}:${tag} on ${image_dir}${CEND}"
-    docker build -t ${USER}/${image_name}:${tag} ${image_dir}
-    if [ $? == 0 ]; then
-      echo -e "${CGREEN}                       ---                                   "
-      echo -e "Successfully built ${USER}/${image_name}:${tag} with context ${image_dir}"
-      echo -e "                       ---                                   ${CEND}"
-      if [ "$DOCKER_PUSH" == "push" ]; then
-        echo -e "${CYELLOW}Push ${USER}/${image_name}:${tag}${CEND}"
-        docker push ${USER}/${image_name}:${tag}
-        echo -e "${CYELLOW}                       ---                                   "
-        echo -e "Successfully push ${USER}/${image_name}:${tag}"
-        echo -e "                       ---                                   ${CEND}"
-      fi
-    else
-      echo -e "${CRED}                       ---                                   "
-      echo -e "Failed built ${USER}/${image_name}:${tag} with context ${image_dir}"
-      echo -e "                       ---                                   ${CEND}"
-      exit 1
-    fi
-  done
-}
-
-
-# get the dockerfiles changed
-IFS=$'\n'
-files=( $(validate_diff --diff-filter=ACMRTUX --name-only -- '*Dockerfile') )
-unset IFS
 
 # build the changed dockerfiles
-for f in "${files[@]}"; do
-    image=${f%Dockerfile}
-    base=${image%%\/*}
-    build_dir=$(dirname $f)
-    build_base=$(echo $build_dir | cut -d"/" -f1)
-
-		if [ -e ${build_base}/custom.sh ]; then
-			echo -e "${CBLUE}                       ---                                   "
-			echo -e "Build ${build_dir} with custom.sh"
-			echo -e "                       ---                                   ${CEND}"
-			chmod +x ${build_base}/custom.sh
-			./${build_base}/custom.sh $DOCKER_PUSH
-			if [ $? == 0 ]; then
-				echo -e "${CGREEN}                       ---                                   "
-				echo -e "Successfully built ${build_dir} with custom.sh"
-				echo -e "                       ---                                   ${CEND}"
-			else
-				echo -e "${CRED}                       ---                                   "
-	      echo -e "Failed built ${build_dir} with custom.sh"
-	      echo -e "                       ---                                   ${CEND}"
-	      exit 1
-			fi
-	  else
-    	build_image ${base} ${build_dir}
-	  fi
+for f in $(git diff HEAD~ "$@" --diff-filter=ACMRTUX --name-only | cut -d"/" -f1 | uniq); do
+    if [ -d $f ]; then
+        if [ ! -x $f/build.sh ]; then
+            chmod +x $f/build.sh
+        fi
+        ./$f/build.sh $DOCKER_PUSH
+    fi
 done
