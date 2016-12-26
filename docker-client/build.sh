c9d348fd64af2e5a4e18683fd4d1ddf0f6d64b14
xataz <xataz@mondedie.fr>
2016-12-26 16:05:41 +0100

    Update(docker-client) : Edit startup

--- docker-client/build.sh
+++ docker-client/build.sh
@@ -6,16 +6,7 @@ DOCKER_PUSH=$1
 # Download dependencies
 docker pull xataz/alpine:3.4
 
-# Build rutorrent
-echo "Build with docker 1.9.1"
-docker build --build-arg DOCKER_VER=1.9.1 -t xataz/docker-client:1.9.1 $FOLDER
-docker tag xataz/docker-client:1.9.1 xataz/docker-client:1.9
-echo "Build with docker 1.10.3"
-docker build --build-arg DOCKER_VER=1.10.3 -t xataz/docker-client:1.10.3 $FOLDER
-docker tag xataz/docker-client:1.10.3 xataz/docker-client:1.10
-echo "Build with docker 1.11.2"
-docker build --build-arg DOCKER_VER=1.11.2 -t xataz/docker-client:1.11.2 $FOLDER
-docker tag xataz/docker-client:1.11.2 xataz/docker-client:1.11
+# Build docker-client
 echo "Build with docker 1.12.5"
 docker build --build-arg DOCKER_VER=1.12.5 -t xataz/docker-client:1.12.5 $FOLDER
 docker tag xataz/docker-client:1.12.5 xataz/docker-client:1.12
