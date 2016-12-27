32fb77bb5850b43b6eeb3d2f868e1fbbb25c6fd2
xataz <xataz@mondedie.fr>
2016-12-27 07:44:56 +0100

    Update(rtorrent-rutorrent) : Edit build.sh

--- rtorrent-rutorrent/build.sh
+++ rtorrent-rutorrent/build.sh
@@ -3,16 +3,30 @@
 FOLDER=$(dirname $0)
 DOCKER_PUSH=$1
 
+f_log() {
+  echo "=$1= $(date +%d/%m/%Y-%H:%M:%S) $2"
+}
+
 # Download dependencies
 docker pull xataz/alpine:3.4
 
 # Build rtorrent-rutorrent
-docker build -t xataz/rtorrent-rutorrent:latest $FOLDER
-docker build --build-arg WITH_FILEBOT=YES -t xataz/rtorrent-rutorrent:filebot $FOLDER
-docker tag xataz/rtorrent-rutorrent:filebot xataz/rtorrent-rutorrent:latest-filebot
-
-if [ "$DOCKER_PUSH" == "push" ]; then
-  for tag in latest filebot latest-filebot; do
-    docker push xataz/rtorrent-rutorrent:$tag
-  done
+f_log INF "Build xataz/rtorrent-rutorrent:latest ..."
+docker build -t xataz/rtorrent-rutorrent:latest $FOLDER > /tmp/build.log 2>&1
+if [ $? == 0 ]; then
+  f_log INF "Build xataz/rtorrent-rutorrent:latest done"
+  [ "$DOCKER_PUSH" == "push" ] && docker push xataz/rtorrent-rutorrent:latest
+else
+  f_log ERR "Build xataz/rtorrent-rutorrent:latest failed"
+  echo /tmp/build.log
+fi
+f_log INF "Build xataz/rtorrent-rutorrent:filebot ..."
+docker build --build-arg WITH_FILEBOT=YES -t xataz/rtorrent-rutorrent:filebot $FOLDER > /tmp/build.log 2>&1
+if [ $? == 0 ]; then
+  f_log INF "Build xataz/rtorrent-rutorrent:filebot done"
+  docker tag xataz/rtorrent-rutorrent:filebot xataz/rtorrent-rutorrent:latest-filebot
+  [ "$DOCKER_PUSH" == "push" ] && docker push xataz/rtorrent-rutorrent:filebot && docker push xataz/rtorrent-rutorrent:latest-filebot
+else
+  f_log ERR "Build xataz/rtorrent-rutorrent:filebot failed"
+  echo /tmp/build.log
 fi
