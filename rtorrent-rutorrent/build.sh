76f1f28d560f1558ccd6872d5b4fb76da8f7efa8
xataz <xataz@mondedie.fr>
2016-12-27 08:27:25 +0100

    Fix(rtorrent-rutorrent) : Fix build error with filebot

--- rtorrent-rutorrent/build.sh
+++ rtorrent-rutorrent/build.sh
@@ -15,18 +15,37 @@ f_log INF "Build xataz/rtorrent-rutorrent:latest ..."
 docker build -t xataz/rtorrent-rutorrent:latest $FOLDER > /tmp/build.log 2>&1
 if [ $? == 0 ]; then
   f_log INF "Build xataz/rtorrent-rutorrent:latest done"
-  [ "$DOCKER_PUSH" == "push" ] && docker push xataz/rtorrent-rutorrent:latest
+  if [ "$DOCKER_PUSH" == "push" ]; then
+    f_log INF "Push xataz/rtorrent-rutorrent:latest ..."
+    docker push xataz/rtorrent-rutorrent:latest > /tmp/push.log 2>&1
+    if [ $? == 0 ]; then
+      f_log INF "Push xataz/rtorrent-rutorrent:latest done"
+    else
+      f_log ERR "Push xataz/rtorrent-rutorrent:latest failed"
+      cat /tmp/push.log
+    fi
+  fi
 else
   f_log ERR "Build xataz/rtorrent-rutorrent:latest failed"
-  echo /tmp/build.log
+  cat /tmp/build.log
 fi
 f_log INF "Build xataz/rtorrent-rutorrent:filebot ..."
 docker build --build-arg WITH_FILEBOT=YES -t xataz/rtorrent-rutorrent:filebot $FOLDER > /tmp/build.log 2>&1
 if [ $? == 0 ]; then
   f_log INF "Build xataz/rtorrent-rutorrent:filebot done"
   docker tag xataz/rtorrent-rutorrent:filebot xataz/rtorrent-rutorrent:latest-filebot
-  [ "$DOCKER_PUSH" == "push" ] && docker push xataz/rtorrent-rutorrent:filebot && docker push xataz/rtorrent-rutorrent:latest-filebot
+    if [ "$DOCKER_PUSH" == "push" ]; then
+    f_log INF "Push xataz/rtorrent-rutorrent:filebot ..."
+    docker push xataz/rtorrent-rutorrent:filebot > /tmp/push.log 2>&1
+    docker push xataz/rtorrent-rutorrent:latest-filebot >> /tmp/push.log 2>&1
+    if [ $? == 0 ]; then
+      f_log INF "Push xataz/rtorrent-rutorrent:filebot done"
+    else
+      f_log ERR "Push xataz/rtorrent-rutorrent:filebot failed"
+      cat /tmp/push.log
+    fi
+  fi
 else
   f_log ERR "Build xataz/rtorrent-rutorrent:filebot failed"
-  echo /tmp/build.log
+  cat /tmp/build.log
 fi
