80e3abb32288a291d4a8eba8bea85aaefa148c5c
xataz <xataz@mondedie.fr>
2017-01-09 08:43:58 +0100

    Update(manage-this-node) : Upgrade Dockerfile && startup

--- build.sh
+++ build.sh
@@ -25,22 +25,22 @@ for f in $(git diff HEAD~ --diff-filter=ACMRTUX --name-only | cut -d"/" -f1 | un
             for dockerfile in $(find $f -name Dockerfile); do
                 FOLDER=$(dirname $dockerfile)
                 LOG_FILE="/tmp/${f}_$(date +%Y%m%d).log"
-                echo -e "Build $dockerfile with context $FOLDER on tmp-build [${CYELLOW}..${CEND}]"
-                docker build -f $dockerfile -t tmp-build $FOLDER > $LOG_FILE 2>&1
+                echo -e "Build $dockerfile with context $FOLDER on tmp-build-$f [${CYELLOW}..${CEND}]"
+                docker build -f $dockerfile -t tmp-build-$f $FOLDER > $LOG_FILE 2>&1
                 if [ $? != 0 ]; then
-                    echo -e "Build $dockerfile with context $FOLDER on tmp-build [${CRED}KO${CEND}]"
+                    echo -e "Build $dockerfile with context $FOLDER on tmp-build-$f [${CRED}KO${CEND}]"
                     ERROR=1
                     cat $LOG_FILE
                 else
-                    echo -e "Build $dockerfile with context $FOLDER on tmp-build [${CGREEN}OK${CEND}]"
+                    echo -e "Build $dockerfile with context $FOLDER on tmp-build-$f [${CGREEN}OK${CEND}]"
                     for tag in $(grep "tags=" $dockerfile | cut -d'"' -f2); do
-                        echo -e "Tags tmp-build to ${USER}/${f}:${tag} [${CYELLOW}..${CEND}]"
-                        docker tag tmp-build ${USER}/${f}:${tag}
+                        echo -e "Tags tmp-build-$f to ${USER}/${f}:${tag} [${CYELLOW}..${CEND}]"
+                        docker tag tmp-build-$f ${USER}/${f}:${tag}
                         if [ $? != 0 ]; then
-                            echo -e "Tags tmp-build to ${USER}/${f}:${tag} [${CRED}KO${CEND}]"
+                            echo -e "Tags tmp-build-$f to ${USER}/${f}:${tag} [${CRED}KO${CEND}]"
                             ERROR=1
                         else
-                            echo -e "Tags tmp-build to ${USER}/${f}:${tag} [${CGREEN}OK${CEND}]"
+                            echo -e "Tags tmp-build-$f to ${USER}/${f}:${tag} [${CGREEN}OK${CEND}]"
                             if [ "$DOCKER_PUSH" == "push" ]; then
                                 echo -e "Push ${USER}/${f}:${tag} [${CYELLOW}..${CEND}]"
                                 docker push ${USER}/${f}:${tag} > $LOG_FILE 2>&1
@@ -56,8 +56,8 @@ for f in $(git diff HEAD~ --diff-filter=ACMRTUX --name-only | cut -d"/" -f1 | un
                     done
                 fi
             done
-            docker images | grep tmp-build > /dev/null 2>&1
-            if [ $? -eq 0 ]; then docker rmi tmp-build; fi
+            docker images | grep tmp-build-$f > /dev/null 2>&1
+            if [ $? -eq 0 ]; then docker rmi tmp-build-$f; fi
         fi
         
     fi
